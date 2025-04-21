b9dc2d12463eba800d6b72f74333a70f9603a91b
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2025-04-20 22:42:11 -0400

    styling

--- src/components/Leaderboard.svelte
+++ src/components/Leaderboard.svelte
@@ -13,16 +13,16 @@
 		</thead>
 		<tbody>
 			{#each db.WAs.sort((a, b) => b.partners - a.partners) as WA}
-				<tr>
+				<tr key={WA.name} class="leaderboard-row">
+					<!-- <td>
+						<img src={WA.logo} alt={WA.name} width="50" height="50" />
+					</td> -->
 					<td>{WA.name}</td>
 					<td>{WA.partners}</td>
 				</tr>
 			{/each}
 		</tbody>
 	</table>
-	<!-- {#each db.WAs.sort((a, b) => b.partners - a.partners) as WA}
-		<p>{WA.name} - {WA.partners}</p>
-	{/each} -->
 </div>
 
 <style>
@@ -33,5 +33,20 @@
 		margin: 10px;
 		background: #f0f0f0;
 		color: #333;
+	}
+
+	td:nth-child(2) {
+		padding: 10px;
+		text-align: center;
+	}
+
+	.leaderboard-row {
+		cursor: pointer;
+		border-radius: 20px;
+	}
+
+	.leaderboard-row:hover {
+		background-color: #e0e0e0;
+		transition: background-color 0.3s ease;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,31 @@
 	import Progress from "../components/Progress.svelte";
 </script>
 
+<svelte:head>
+	<title>GCO Dashboard</title>
+	<meta name="description" content="GCO Dashboard for Contract Year 3" />
+	<link rel="icon" href="/favicon.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="theme-color" content="#ffffff" />
+	<meta property="og:title" content="GCO Dashboard" />
+	<meta property="og:description" content="GCO Dashboard for Contract Year 3" />
+</svelte:head>
+
 <div>
 	<h1>GCO Dashboard</h1>
 	<p>Welcome to GCO Contract Year 3! Check your progress below!</p>
 </div>
 
-<Progress />
-<Leaderboard />
+<section>
+	<Progress />
+	<Leaderboard />
+</section>
 
 <style>
+	section {
+		display: flex;
+		width: 100%;
+	}
 	* {
 		font-family: Arial, sans-serif;
 	}
