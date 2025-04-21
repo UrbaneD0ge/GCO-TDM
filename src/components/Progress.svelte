1ae211e480b8be7ee9bc00ee7a28814f0a2b1d7b
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2025-04-20 20:29:58 -0400

    there is a progress bar

---
+++ src/components/Progress.svelte
@@ -0,0 +1,42 @@
+<script>
+	let progress = 30;
+</script>
+
+<div>
+	<h1>Progress</h1>
+
+	<!-- Progress Meter goes here -->
+	<div id="progress"></div>
+</div>
+
+<style>
+	:root {
+		--progress: 30%;
+	}
+
+	div {
+		border: #333 solid 1px;
+		border-radius: 10px;
+		padding: 10px;
+		margin: 10px;
+		background: #f0f0f0;
+		color: #333;
+	}
+
+	#progress {
+		background: #333;
+		border-radius: 13px;
+		height: 20px;
+		width: 300px;
+		padding: 3px;
+	}
+
+	#progress:after {
+		content: "";
+		display: block;
+		background: orange;
+		width: var(--progress);
+		height: 100%;
+		border-radius: 9px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Progress from "../components/Progress.svelte";
+</script>
+
+<div>
+	<h1>GCO Dashboard</h1>
+	<p>Welcome to GCO Contract Year 3! Check your progress below!</p>
+
+	<Progress />
+</div>
+
+<style>
+	* {
+		font-family: Arial, sans-serif;
+	}
+	div {
+		margin: 0;
+		padding: 0;
+		background: #f0f0f0;
+	}
+</style>
